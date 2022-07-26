54379ced6cfba7ffe100872789d96e1a4bc02ec5
teocollin1995 <teoc@mit.edu>
2022-07-26 15:50:01 -0400

    forgot to add auxtypes

---
+++ auxtypes.py
@@ -0,0 +1,21 @@
+from typing import *
+
+stampsdict = dict()
+class stamp:
+    __slots__ = ('__weakref__', 'stamp', "args")
+    def __init__(self, args=None):
+        h = hash(args)
+        if h in stampsdict:
+            stampsdict[h] += 1
+        else:
+            stampsdict[h] = 1
+        self.stamp = stampsdict[h]
+        self.args = args
+    def __hash__(self):
+        return self.stamp
+    def __repr__(self):
+        return "@{0}".format(self.stamp)
+    def __copy__(self):
+        return stamp(args=self.args)
+    def __deepcopy__(self):
+        return stamp(args=self.args)
