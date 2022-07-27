51f5cd526afa5da3774d53d196aeef3dee6b1c30
teocollin1995 <teoc@mit.edu>
2022-07-27 10:42:23 -0400

    flake8

--- ADT/auxtypes.py
+++ ADT/auxtypes.py
@@ -1,8 +1,10 @@
-from typing import *
 
 stampsdict = dict()
+
+
 class stamp:
     __slots__ = ('__weakref__', 'stamp', "args")
+
     def __init__(self, args=None):
         h = hash(args)
         if h in stampsdict:
@@ -11,13 +13,19 @@ class stamp:
             stampsdict[h] = 1
         self.stamp = stampsdict[h]
         self.args = args
+
     def __hash__(self):
         return self.stamp
+
     def __repr__(self):
         return "@{0}".format(self.stamp)
+
     def __copy__(self):
         return stamp(args=self.args)
+
     def __deepcopy__(self):
         return stamp(args=self.args)
+
+
 def defaultStamp():
     return stamp()
